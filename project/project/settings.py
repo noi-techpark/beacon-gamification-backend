228aefe8a3d13c3536737635a58e8623caf31b9c
lucafedri <9001053+fedriz@users.noreply.github.com>
2019-11-05 14:48:02 +0100

    add AWS_S3_SIGNATURE_VERSION

--- project/project/settings.py
+++ project/project/settings.py
@@ -140,6 +140,7 @@ CORS_ORIGIN_ALLOW_ALL = DEBUG
 
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 AWS_STORAGE_BUCKET_NAME = 'beacon-app-bucket'
+AWS_S3_SIGNATURE_VERSION = 's3v4'
 
 if not DEBUG:
     with open('/home/secrets/AWS_ACCESS_KEY_ID.txt') as f:
