450da7f9e26f79a8c3589bfecf07528dead87d2e
lucafedri <9001053+fedriz@users.noreply.github.com>
2019-11-05 14:58:51 +0100

    Add AWS_S3_REGION_NAME

--- project/project/settings.py
+++ project/project/settings.py
@@ -141,6 +141,7 @@ CORS_ORIGIN_ALLOW_ALL = DEBUG
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 AWS_STORAGE_BUCKET_NAME = 'beacon-app-bucket'
 AWS_S3_SIGNATURE_VERSION = 's3v4'
+AWS_S3_REGION_NAME = 'eu-central-1'
 
 if not DEBUG:
     with open('/home/secrets/AWS_ACCESS_KEY_ID.txt') as f:
