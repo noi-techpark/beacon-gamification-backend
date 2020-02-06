5bea8d20e4291667fe6afb0c4cd3ad2749c5da3a
Alex Lanz <alex@krumer.it>
2020-02-06 06:55:16 +0100

    Deployment

--- Jenkinsfile-CI.groovy
+++ Jenkinsfile-CI.groovy
@@ -11,7 +11,7 @@ pipeline {
             steps {
 				sh '''
 					docker-compose --no-ansi build --pull --build-arg JENKINS_USER_ID=$(id -u jenkins) --build-arg JENKINS_GROUP_ID=$(id -g jenkins)
-					docker-compose --no-ansi run --rm --no-deps -u $(id -u jenkins):$(id -g jenkins) app "python3 --version"
+					docker-compose --no-ansi run --rm --no-deps -u $(id -u jenkins):$(id -g jenkins) app python --version
 				'''
             }
         }
